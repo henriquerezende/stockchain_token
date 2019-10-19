ce5d73cfcfe283e608e79bb668f624f8ba30de0c
Henrique Rezende <henriquerezendec@gmail.com>
2019-10-18 23:23:04 -0300

    inicio

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,20 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-    // constructor
-    // set the total number of tokens
-    // read the total number of tokens
+    //name
+    string public name = 'DApp Token';
+    string public symbol = 'DAPP';
+    string public standard = 'DAPP Token v1.0';
+
     uint256 public totalSupply;
 
-    constructor () public {
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        //allocate the initial supply
+
+
     }
 }
